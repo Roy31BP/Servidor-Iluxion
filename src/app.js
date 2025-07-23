e550184984e01dec1bdcae478a41a5165c919c28
Roy31BP <roy31briseno@gmail.com>
2025-07-23 12:31:43 -0600

    nueva ruta

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const app = express();
 
 // Lista de orígenes permitidos
-const allowedOrigins = ['http://localhost:5174', 'http://localhost:5173', 'https://iluxion-clientes.onrender.com'];
+const allowedOrigins = ['http://localhost:5174', 'http://localhost:5173', 'https://iluxion-clientes.onrender.com', 'https://admin-react-emh6.onrender.com'];
 // Configuración de CORS
 
 
